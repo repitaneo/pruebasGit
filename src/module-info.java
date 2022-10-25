3105479d0e221bef68c24fcf13545eb96969847c
Marcos Fernandez <marcosd@educastur.org>
2022-10-25 09:04:15 +0200

    primer commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Marcos
+ *
+ */
+module pruebasGit {
+}
---
+++ src/pruebasGit/Start.java
@@ -0,0 +1,12 @@
+package pruebasGit;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hola mundo");
+	}
+
+}
